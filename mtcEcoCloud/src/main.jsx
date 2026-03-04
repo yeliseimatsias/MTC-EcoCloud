30e0612f90975f239cb1eea1277c778fc712735e
Rudz Ilya <ilarud974@gmail.com>
2026-03-04 16:09:23 +0300

    Add index.css

--- mtcEcoCloud/src/main.jsx
+++ mtcEcoCloud/src/main.jsx
@@ -1,8 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Authorization from './Authorization.jsx'
+import './css/index.css'
 
 
 createRoot(document.getElementById('root')).render(
-  <Authorization />
+ // <Authorization />
+  <App />
 )
