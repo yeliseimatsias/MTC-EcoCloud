a6763846abcbfa3a8077b09ad4dfa1341234d9e7
Rudz Ilya <ilarud974@gmail.com>
2026-03-04 15:03:58 +0300

    Add authorization.jsx file

---
+++ mtcEcoCloud/src/Authorization.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function Authorization() {
+
+  return (
+    <p>test</p>
+  )
+}
+
+export default Authorization
--- mtcEcoCloud/src/main.jsx
+++ mtcEcoCloud/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Authorization from './Authorization.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  <Authorization />
 )
